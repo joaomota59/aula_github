e7b658b47c74789576cdcf9febc594d54e177b43
michael <mlmbufpi@gmail.com>
2021-05-08 22:47:13 -0300

    Atividade #12 concluida

---
+++ Conta.java
@@ -0,0 +1,23 @@
+public class Conta {
+    protected int numero; 
+    protected String dono;
+    protected double saldo; 
+
+    public double getSaldo(){
+        return this.saldo;
+    }
+
+    void deposita(double quantidade){
+        this.saldo += quantidade;
+    }
+
+    void saca(double quantidade){
+        double novoSaldo = this.saldo - quantidade;
+        this.saldo = novoSaldo;
+    }
+
+    void transfere(Conta destino, double valor){
+        this.saldo = this.saldo - valor; 
+        destino.saldo = destino.saldo + valor; 
+    }
+}
